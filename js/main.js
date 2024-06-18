3ba32543ac28abc8558f6c15737137bc1b1e8737
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-18 15:39:46 +0200

    feat: add team members information in html webpage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+//selezionare l'elemento di markup dove si troveranno le informazioni
+const contentTable = document.getElementById("table");
+
 //Creare l’array di oggetti con le informazioni fornite.
 const arrTeam = [
     {
@@ -40,18 +43,48 @@ const arrTeam = [
 
 console.log(arrTeam);
 
-//estrapolare l'array
+// Creare la struttura della tabella
+let tableHTML = `
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Image</th>
+            </tr>
+        </thead>
+        <tbody>
+    `;
 
+//estrapolare gli oggetti dell'array
 for(let i = 0; i < arrTeam.length; i++){
 
     let members = arrTeam[i];
     console.log(members);
 
-    //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+    //estrarre tutte le proprietà degli oggetti
     for (let key in members){
-        console.log(key, " : ", members[key]);
+        console.log(key, " : ", members[key]);//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
     }
+
+    //Stampare le stesse informazioni su DOM sottoforma di stringhe
+    tableHTML += `
+        <tr>
+            <td>${members.name}</td>
+            <td>${members.role}</td>
+            <td>${members.image}</td>
+        </tr>
+    `;
+
 
 }
 
-//Stampare le stesse informazioni su DOM sottoforma di stringhe
+// Chiudere la tabella
+tableHTML += `
+    </tbody>
+</table>
+`;
+
+// Aggiungere la tabella al contenitore
+contentTable.innerHTML = tableHTML;
