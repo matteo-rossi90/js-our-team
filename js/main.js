bb99be6c2b1976b41a7e09664054373f4b55a6a4
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-18 14:54:53 +0200

    feat: add js code to create an array to contain team members information

--- js/main.js
+++ js/main.js
@@ -4,7 +4,54 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*/
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 //Creare l’array di oggetti con le informazioni fornite.
+const arrTeam = [
+    {
+        "name": "Wayne Barnet",
+        "role": "Founder & CEO",
+        "image": "wayne-barnet-founder-ceo.jpg"
+    },
+    {
+        "name": "Angela Caroll",
+        "role": "Chief Editor",
+        "image": "angela-caroll-chief-editor.jpg"
+    },
+    {
+        "name": "Walter Gordon",
+        "role": "Office manager",
+        "image": "walter-gordon-office-manager.jpg"
+    },
+    {
+        "name": "Angela Lopez",
+        "role": "Social Media Manager",
+        "image": "angela-lopez-social-media.jpg"
+    },
+    {
+        "name": "Scott Estrada",
+        "role": "Developer",
+        "image": "scott-estrada-developer.jpg"
+    },
+    {
+        "name": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "image": "barbara-ramos-graphic-designer.jpg"
+    }
 
-//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+];
+
+console.log(arrTeam);
+
+//estrapolare l'array
+
+for(let i = 0; i < arrTeam.length; i++){
+
+    let members = arrTeam[i];
+    console.log(members);
+
+    //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+    for (let key in members){
+        console.log(key, " : ", members[key]);
+    }
+
+}
 
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
